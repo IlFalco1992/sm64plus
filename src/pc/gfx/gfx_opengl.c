d83f869cb72f191485d376dcc04838121868a50e
Francesco Scotti <francesco.scotti92@gmail.com>
2023-02-05 23:52:25 +0100

    SDL_opengl_glext.h header

--- src/pc/gfx/gfx_opengl.c
+++ src/pc/gfx/gfx_opengl.c
@@ -21,7 +21,9 @@
 #ifndef TARGET_MACOS
 #include <SDL2/SDL.h>
 #else
+#define GL_GLEXT_PROTOTYPES 1
 #include <SDL_opengl.h>
+#include <SDL_opengl_glext.h>
 #include <stdio.h>
 #endif
 #define GL_GLEXT_PROTOTYPES 1
